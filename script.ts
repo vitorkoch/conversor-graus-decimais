52413ff4850a7bdeb1422baed559900c8967762e
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-06-06 18:36:08 -0300

    update

--- script.ts
+++ script.ts
@@ -1,5 +1,17 @@
 const campoGrausX = document.getElementById('grausX');
 
+/*
+function valido(array) {
+    for(let i in array) {
+        if(array[i]) {
+            return true
+        } else {
+            return false
+        }
+    }
+}*/
+
+
 function converter() {}
 
 function deletar(id) {
