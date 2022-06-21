4fe585e72f3e4e5f023a51d112df29ae355aaea4
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-06-21 20:15:13 -0300

    mudança de idioma

--- graus-decimais/script.js
+++ graus-decimais/script.js
@@ -1,9 +1,9 @@
-const fieldGrausX = document.getElementById('grausX');
-const fieldGrausY = document.getElementById('grausY');
-const fieldMinutosX = document.getElementById('minutosX');
-const fieldMinutosY = document.getElementById('minutosY');
-const fieldSegundosX = document.getElementById('segundosX');
-const fieldSegundosY = document.getElementById('segundosY');
+const fielddegreesX = document.getElementById('degreesX');
+const fielddegreesY = document.getElementById('degreesY');
+const fieldminutesX = document.getElementById('minutesX');
+const fieldminutesY = document.getElementById('minutesY');
+const fieldsecondsX = document.getElementById('secondsX');
+const fieldsecondsY = document.getElementById('secondsY');
 const res = document.getElementById('res');
 const longitude = document.getElementsByName('longitude');
 const latitude = document.getElementsByName('latitude');
@@ -11,7 +11,7 @@ document.getElementById('botao').addEventListener('click', converter);
 function valor(field) {
     return Number(field instanceof HTMLInputElement ? field.value : '');
 }
-function validar(field) {
+function validate(field) {
     if (valor(field) >= 0)
         return true;
     else
@@ -31,19 +31,19 @@ function converter() {
     else {
         var sinalX = '-';
     }
-    if (validar(fieldGrausX) &&
-        validar(fieldGrausY) &&
-        validar(fieldMinutosX) &&
-        validar(fieldMinutosY) &&
-        validar(fieldSegundosX) &&
-        validar(fieldSegundosY)) {
-        const grausY = valor(fieldGrausY);
-        const minutosY = valor(fieldMinutosY);
-        const segundosY = valor(fieldSegundosY);
-        const grausX = valor(fieldGrausX);
-        const minutosX = valor(fieldMinutosX);
-        const segundosX = valor(fieldSegundosX);
-        res.innerHTML = `y: ${sinalY + (grausY + minutosY / 60 + segundosY / 3600).toPrecision(5)}<br>x: ${sinalX + (grausX + minutosX / 60 + segundosX / 3600).toPrecision(5)}`;
+    if (validate(fielddegreesX) &&
+        validate(fielddegreesY) &&
+        validate(fieldminutesX) &&
+        validate(fieldminutesY) &&
+        validate(fieldsecondsX) &&
+        validate(fieldsecondsY)) {
+        const degreesY = valor(fielddegreesY);
+        const minutesY = valor(fieldminutesY);
+        const secondsY = valor(fieldsecondsY);
+        const degreesX = valor(fielddegreesX);
+        const minutesX = valor(fieldminutesX);
+        const secondsX = valor(fieldsecondsX);
+        res.innerHTML = `y: ${sinalY + (degreesY + minutesY / 60 + secondsY / 3600).toPrecision(5)}<br>x: ${sinalX + (degreesX + minutesX / 60 + secondsX / 3600).toPrecision(5)}`;
     }
     else {
         alert('Valores inválidos! Tente novamente');
--- graus-decimais/script.ts
+++ graus-decimais/script.ts
@@ -1,9 +1,9 @@
-const fieldGrausX = document.getElementById('grausX');
-const fieldGrausY = document.getElementById('grausY');
-const fieldMinutosX = document.getElementById('minutosX');
-const fieldMinutosY = document.getElementById('minutosY');
-const fieldSegundosX = document.getElementById('segundosX');
-const fieldSegundosY = document.getElementById('segundosY');
+const fielddegreesX = document.getElementById('degreesX');
+const fielddegreesY = document.getElementById('degreesY');
+const fieldminutesX = document.getElementById('minutesX');
+const fieldminutesY = document.getElementById('minutesY');
+const fieldsecondsX = document.getElementById('secondsX');
+const fieldsecondsY = document.getElementById('secondsY');
 const res = document.getElementById('res');
 const longitude = document.getElementsByName('longitude');
 const latitude = document.getElementsByName('latitude');
@@ -13,7 +13,7 @@ document.getElementById('botao').addEventListener('click', converter);
 function valor(field) {
     return Number(field instanceof HTMLInputElement ? field.value : '');
 }
-function validar(field) {
+function validate(field) {
     if (valor(field) >= 0) return true;
     else return false;
 }
@@ -30,23 +30,23 @@ function converter() {
         var sinalX = '-';
     }
     if (
-        validar(fieldGrausX) &&
-        validar(fieldGrausY) &&
-        validar(fieldMinutosX) &&
-        validar(fieldMinutosY) &&
-        validar(fieldSegundosX) &&
-        validar(fieldSegundosY)
+        validate(fielddegreesX) &&
+        validate(fielddegreesY) &&
+        validate(fieldminutesX) &&
+        validate(fieldminutesY) &&
+        validate(fieldsecondsX) &&
+        validate(fieldsecondsY)
     ) {
-        const grausY = valor(fieldGrausY);
-        const minutosY = valor(fieldMinutosY);
-        const segundosY = valor(fieldSegundosY);
-        const grausX = valor(fieldGrausX);
-        const minutosX = valor(fieldMinutosX);
-        const segundosX = valor(fieldSegundosX);
+        const degreesY = valor(fielddegreesY);
+        const minutesY = valor(fieldminutesY);
+        const secondsY = valor(fieldsecondsY);
+        const degreesX = valor(fielddegreesX);
+        const minutesX = valor(fieldminutesX);
+        const secondsX = valor(fieldsecondsX);
         res.innerHTML = `y: ${
-            sinalY + (grausY + minutosY / 60 + segundosY / 3600).toPrecision(5)
+            sinalY + (degreesY + minutesY / 60 + secondsY / 3600).toPrecision(5)
         }<br>x: ${
-            sinalX + (grausX + minutosX / 60 + segundosX / 3600).toPrecision(5)
+            sinalX + (degreesX + minutesX / 60 + secondsX / 3600).toPrecision(5)
         }`;
     } else {
         alert('Valores inválidos! Tente novamente');
