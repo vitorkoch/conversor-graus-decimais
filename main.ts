57c20df38f02b178376f61cd41d4de7ff3f057c4
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-08-28 14:53:43 -0300

    npm init

---
+++ main.ts
@@ -0,0 +1,21 @@
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        icon: __dirname + '/media/pages.png',
+        show: false,
+    });
+    mainWindow.once("ready-to-show", () => mainWindow.show())
+
+    mainWindow.setTitle("My sites");
+    mainWindow.loadURL("https://my-sites-theta.vercel.app/")
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+});
